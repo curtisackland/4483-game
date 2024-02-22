5b1a982d1d52b5a2fed946ce98a70313a5382d39
Curtman <curtisackland@rogers.com>
2024-02-22 16:11:02 -0500

    Initial commit

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [Header("HealthXP Bar")]
+    private float health;
+
+    public float maxHealth = 100;
+
+    public float chipSpeed = 2f;
+
+    private float lerpTimer;
+
+    public Image frontHealthBar;
+    
+    public Image backHealthBar;
+
+    [Header("Damage Overlay")]
+    public Image overlay;
+
+    public float duration;
+
+    public float fadeSpeed;
+
+    private float durationTimer;
+    
+    void Start()
+    {
+        health = maxHealth;
+        overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, 0);
+    }
+    
+    void Update()
+    {
+        health = Mathf.Clamp(health, 0, maxHealth);
+        UpdateHealthUI();
+        if (overlay.color.a > 0)
+        {
+            durationTimer += Time.deltaTime;
+            if (durationTimer > duration)
+            {
+                float tempAlpha = overlay.color.a;
+                tempAlpha -= Time.deltaTime * fadeSpeed;
+                overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, tempAlpha);
+            }
+        }
+    }
+
+    public void UpdateHealthUI()
+    {
+        float fillFront = frontHealthBar.fillAmount;
+        float fillBack = backHealthBar.fillAmount;
+        float healthFraction = health / maxHealth;
+        if (fillBack > healthFraction)
+        {
+            frontHealthBar.fillAmount = healthFraction;
+            backHealthBar.color = Color.red;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete = percentComplete * percentComplete; // makes animation look smoother
+            backHealthBar.fillAmount = Mathf.Lerp(fillBack, healthFraction, percentComplete);
+        } else if (fillFront < healthFraction)
+        {
+            backHealthBar.fillAmount = healthFraction;
+            backHealthBar.color = Color.green;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete = percentComplete * percentComplete; // makes animation look smoother
+            frontHealthBar.fillAmount = Mathf.Lerp(fillFront, healthFraction, percentComplete);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        lerpTimer = 0f;
+        overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, 0.5f);
+    }
+
+    public void RestoreHealth(float healAmount)
+    {
+        health += healAmount;
+        lerpTimer = 0f;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public Camera cam;
+
+    private float xRotation = 0f;
+
+    public float xSensitivity = 30f;
+
+    public float ySensitivty = 30f;
+
+    public void ProcessLook(Vector2 input)
+    {
+        float mouseX = input.x;
+        float mouseY = input.y;
+        
+        // camera rotation for looking up and down
+        xRotation -= (mouseY * Time.deltaTime) * ySensitivty;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+        
+        // apply rotation to camera
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        
+        // camera rotation for left and right
+        transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSensitivity);
+    }
+    
+}
