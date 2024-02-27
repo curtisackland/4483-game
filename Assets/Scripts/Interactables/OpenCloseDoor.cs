78745ffff53a1cfd8f065f516e7a9ba9b355c377
Curtman <curtisackland@rogers.com>
2024-02-26 19:46:20 -0500

    Added Home and door animation

---
+++ Assets/Scripts/Interactables/OpenCloseDoor.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCloseDoor : Interactable
+{
+    [SerializeField]
+    private GameObject door;
+
+    private bool doorOpen;
+    
+    protected override void Interact()
+    {
+        doorOpen = !doorOpen;
+        door.GetComponent<Animator>().SetBool("IsOpen", doorOpen);
+    }
+}
