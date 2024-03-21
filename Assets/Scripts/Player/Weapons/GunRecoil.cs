51667796552633bddd33d280fa5855914dc6c07a
Curtman <curtisackland@rogers.com>
2024-03-21 00:31:05 -0400

    Added recoil animations

--- Assets/Scripts/Player/Weapons/GunRecoil.cs
+++ Assets/Scripts/Player/Weapons/GunRecoil.cs
@@ -4,26 +4,51 @@ using UnityEngine;
 
 public class GunRecoil : MonoBehaviour
 {
+
+    public GunData gunData;
+    
     private Vector3 currentRotation;
     private Vector3 targetRotation;
+    
+    private float currentRecoilPosition, finalRecoilPosition;
 
-    [SerializeField] private float recoilX;
-    [SerializeField] private float recoilY;
-    [SerializeField] private float recoilZ;
+    private Transform camHolder;
 
-    [SerializeField] private float snappiness;
-
-    [SerializeField] private float returnSpeed;
+    private ScopeIn scopeIn;
     
     void Start()
     {
-        
+        camHolder = transform.parent.parent.parent.transform;
+        scopeIn = GetComponent<ScopeIn>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        targetRotation = Vector3.Lerp(targetRotation, Vector3.zero, returnSpeed * Time.deltaTime);
-        currentRotation = Vector3.Slerp(currentRotation, targetRotation, snappiness * Time.fixedDeltaTime);
+        // bullet recoil
+        targetRotation = Vector3.Lerp(targetRotation, Vector3.zero, gunData.returnSpeed * Time.deltaTime);
+        currentRotation = Vector3.Slerp(currentRotation, targetRotation, gunData.snappiness * Time.fixedDeltaTime);
+        camHolder.SetLocalPositionAndRotation(camHolder.localPosition, Quaternion.Euler(currentRotation));
+        
+        // gun recoil
+        currentRecoilPosition = Mathf.Lerp(currentRecoilPosition, 0, gunData.gunReturnSpeed * Time.deltaTime);
+        finalRecoilPosition = Mathf.Lerp(finalRecoilPosition, currentRecoilPosition, gunData.kickBackSpeed * Time.deltaTime);
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, finalRecoilPosition);
+    }
+
+    public void RecoilFire()
+    {
+        currentRecoilPosition += gunData.kickBackAmount;
+        
+        // ADS and hipfire have different recoils
+        if (scopeIn.isADS())
+        {
+            targetRotation += new Vector3(gunData.adsRecoilX, Random.Range(-gunData.adsRecoilY, gunData.adsRecoilY), Random.Range(-gunData.adsRecoilZ, gunData.adsRecoilZ));
+        }
+        else
+        {
+            targetRotation += new Vector3(gunData.hipFireRecoilX, Random.Range(-gunData.hipFireRecoilY, gunData.hipFireRecoilY), Random.Range(-gunData.hipFireRecoilZ, gunData.hipFireRecoilZ));
+        }
+        
     }
 }
--- Assets/Scripts/ScriptableObjects/GunData.cs
+++ Assets/Scripts/ScriptableObjects/GunData.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [CreateAssetMenu(fileName  = "Gun", menuName = "Weapon/Gun")]
@@ -13,12 +14,27 @@ public class GunData : ScriptableObject
     public float maxDistance;
     public int bulletsPerShot;
     public float bulletSpread;
+    public float bulletSpeed = 200;
 
     [Header("Reloading")]
     public int currentAmmo;
     public int magSize;
     public float fireRate;
     public float reloadTime;
+    
+    [Header("Recoil")]
+    public float hipFireRecoilX = -2.0f;
+    public float hipFireRecoilY = 2.0f;
+    public float hipFireRecoilZ = 0.35f;
+    public float adsRecoilX = -2.0f;
+    public float adsRecoilY = 2.0f;
+    public float adsRecoilZ = 0.35f;
+    public float snappiness = 6.0f;
+    public float returnSpeed = 2.0f;
+    
+    public float kickBackAmount = -0.5f;
+    public float kickBackSpeed = 10;
+    public float gunReturnSpeed = 20;
     
     [HideInInspector]
     public bool reloading;
